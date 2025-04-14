3614f95146e3cc93fd0bbaa57b714c7929515d52
Aayush Verma <aayush.verma@mygurukulam.co>
2025-04-15 04:21:53 +0530

    Create output.tf

---
+++ Network-skeleton/output.tf
@@ -0,0 +1,28 @@
+output "vpc_id" {
+  value       = aws_vpc.otms_vpc.id
+  description = "id of the otms vpc "
+}
+
+output "public_subnet_ids" {
+  value = local.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = local.private_subnet_ids
+}
+
+output "igw_id" {
+  description = "Internet Gateway ID"
+  value       = aws_internet_gateway.IGW.id
+}
+
+output "NAT_Gateway" {
+  value = aws_nat_gateway.NAT_GW.id
+}
+
+output "public_rt_id" {
+  value = aws_route_table.public_rt.id
+}
+output "privat_rt_id" {
+  value = aws_route_table.private_rt.id
+}
