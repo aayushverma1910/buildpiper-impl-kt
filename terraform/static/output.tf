f9562afd6d2df9a4cddecbc929204cf4aca0ec16
Aayush Verma <aayush.verma@mygurukulam.co>
2025-04-15 16:28:11 +0530

    Update output.tf

--- terraform/static/output.tf
+++ terraform/static/output.tf
@@ -60,15 +60,3 @@ output "eks_node_group_role_arn" {
   description = "The ARN of the node group role"
   value       = aws_iam_role.eks_node_role.arn
 }
-
-###################### Outputs for IAM Roles ####################
-
-output "eks_cluster_role_arn" {
-  description = "The ARN of the EKS cluster role"
-  value       = aws_iam_role.eks_cluster_role.arn
-}
-
-output "eks_node_role_arn" {
-  description = "The ARN of the EKS node role"
-  value       = aws_iam_role.eks_node_role.arn
-}
