6de53ef68dac33b75e6979fba8b83b24cb09a553
Aayush Verma <aayush.verma@mygurukulam.co>
2025-04-17 19:41:28 +0530

    Update local.tf

--- terraform/module/compute/local.tf
+++ terraform/module/compute/local.tf
@@ -4,6 +4,17 @@ locals {
   eks_name = "${var.env}-${var.project_name}-eks-cluster"
 }
 
+###################### launch template  ####################
+
+locals {
+  app_lt_name = "${var.env}-${var.project_name}-${var.app_launch_template_name}-lt"
+}
+
+locals {
+  db_lt_name = "${var.env}-${var.project_name}-${var.db_launch_template_name}-lt"
+}
+
+
 ###################### Node Group  ####################
 
 locals {
