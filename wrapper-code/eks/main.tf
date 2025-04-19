760733065c13987cc2858e0832051edc5a835178
Aayush Verma <aayush.verma@mygurukulam.co>
2025-04-19 18:21:31 +0530

    Update main.tf

--- wrapper-code/eks/main.tf
+++ wrapper-code/eks/main.tf
@@ -33,11 +33,7 @@ module "networking_module" {
   public_rt_name     = var.public_rt_name
   private_rt_name    = var.private_rt_name
 
-  # Security Groups 
 
-  create_sg            = var.create_sg
-  sg_names             = var.sg_names
-  security_groups_rule = var.security_groups_rule
 }
 
 module "compute_module" {
@@ -45,25 +41,52 @@ module "compute_module" {
   env                          = var.env
   owner                        = var.owner
   project_name                 = var.project_name
+
+  # cluster  
+
   eks_cluster_version          = var.eks_cluster_version
   eks_cluster_role_name        = var.eks_cluster_role_name
   eks_node_role_name           = var.eks_node_role_name
   endpoint_private_access      = var.endpoint_private_access
   endpoint_public_access       = var.endpoint_public_access
-  app_launch_template_name     = var.app_launch_template_name
-  app_instance_type            = var.app_instance_type
-  db_launch_template_name      = var.db_launch_template_name
-  db_instance_type             = var.db_instance_type
-  ami_id                       = var.ami_id
-  key_name                     = var.key_name
   eks_cluster_role_policy_arns = var.eks_cluster_role_policy_arns
   eks_node_role_policy_arns    = var.eks_node_role_policy_arns
-  node_group_desired_size      = var.node_group_desired_size
-  node_group_max_size          = var.node_group_max_size
-  node_group_min_size          = var.node_group_min_size
-  private_subnet_ids           = module.networking_module.private_subnet_ids
-  application_subnet_ids       = module.networking_module.application_subnet_ids
-  database_subnet_ids          = module.networking_module.database_subnet_ids
-  eks_security_group_ids       = module.networking_module.eks_security_group_ids
+
+  # Security Groups 
+
+  create_sg            = var.create_sg
+  sg_names             = var.sg_names
+  security_groups_rule = var.security_groups_rule
+  eks_sg_rule          = var.eks_sg_rule
+
+  ## Node Group
+
+  ami_type = var.ami_type
+  key_pair = var.key_pair
+
+  #App node group
+
+  app_capacity_type           = var.app_capacity_type
+  app_instance_type           = var.app_instance_type
+  app_disk_size               = var.app_disk_size
+  node_group_app_desired_size = var.node_group_app_desired_size
+  node_group_app_max_size     = var.node_group_app_max_size
+  node_group_app_min_size     = var.node_group_app_min_size
+
+
+  #DB node group
+
+  db_capacity_type           = var.db_capacity_type
+  db_instance_type           = var.db_instance_type
+  db_disk_size               = var.db_disk_size
+  node_group_db_desired_size = var.node_group_db_desired_size
+  node_group_db_max_size     = var.node_group_db_max_size
+  node_group_db_min_size     = var.node_group_db_min_size
+
+
+  vpc_id                 = module.networking_module.vpc_id
+  private_subnet_ids     = module.networking_module.private_subnet_ids
+  application_subnet_ids = module.networking_module.application_subnet_ids
+  database_subnet_ids    = module.networking_module.database_subnet_ids
 
 }
