f065bbabc61d58c3b55a2ab68d562d190e032ba5
Aayush Verma <aayush.verma@mygurukulam.co>
2025-04-20 00:03:42 +0530

    Update main.tf

--- terraform/module/compute/main.tf
+++ terraform/module/compute/main.tf
@@ -77,14 +77,14 @@ resource "aws_security_group" "sg" {
   for_each = var.create_sg ? local.security_group_config : {}
 
   name   = each.value.name
-  vpc_id = var.vpc_id 
+  vpc_id = var.vpc_id
 
   tags = {
     Name  = each.value.name
     env   = var.env
     owner = var.owner
   }
-  depends_on = [ aws_eks_cluster.eks ]
+  depends_on = [aws_eks_cluster.eks]
 }
 
 resource "aws_security_group_rule" "ingress" {
@@ -100,10 +100,10 @@ resource "aws_security_group_rule" "ingress" {
   description       = each.value.rule.description
   security_group_id = aws_security_group.sg[each.value.sg_name].id
 
-  cidr_blocks              = each.value.rule_type == "cidr" ? each.value.rule.cidr_blocks : null
-source_security_group_id = each.value.rule_type == "sg" ? (
-  try(local.known_source_sgs[each.value.rule.source_sg_names[0]], aws_security_group.sg[each.value.rule.source_sg_names[0]].id)
-) : null
+  cidr_blocks = each.value.rule_type == "cidr" ? each.value.rule.cidr_blocks : null
+  source_security_group_id = each.value.rule_type == "sg" ? (
+    try(local.known_source_sgs[each.value.rule.source_sg_names[0]], aws_security_group.sg[each.value.rule.source_sg_names[0]].id)
+  ) : null
 
 
   depends_on = [
@@ -145,11 +145,49 @@ resource "aws_security_group_rule" "sg_to_eks_ingress" {
   source_security_group_id = each.value.source_sg_id
   security_group_id        = aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
 
-  description              = "Allow ${each.key} SG to access EKS Cluster"
+  description = "Allow ${each.key} SG to access EKS Cluster"
 }
 
 
 ###################### APP Node Group  ####################
+
+resource "aws_launch_template" "eks_app_launch_template" {
+  name_prefix   = local.app_lt_name
+  key_name      = var.key_pair != "" ? var.key_pair : null
+  instance_type = var.app_instance_type
+
+
+  network_interfaces {
+    associate_public_ip_address = var.associate_public_ip_app
+    security_groups             = local.app_lt_sg
+  }
+
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name  = "${local.node_group_name}-app"
+      env   = var.env
+      owner = var.owner
+    }
+  }
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size           = var.ebs_app_volume_size
+      volume_type           = var.ebs_app_volume_type
+      delete_on_termination = var.delete_on_termination_app
+      encrypted             = var.app_encrypted
+    }
+  }
+  tags = {
+    Name  = "${local.node_group_name}-app"
+    env   = var.env
+    owner = var.owner
+  }
+  depends_on = [aws_security_group.sg]
+}
 
 
 
@@ -159,29 +197,26 @@ resource "aws_eks_node_group" "app_node_group" {
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = var.application_subnet_ids
 
-  ami_type = var.ami_type
+  ami_type      = var.ami_type
   capacity_type = var.app_capacity_type
-  instance_types = var.app_instance_type
-  disk_size      = var.app_disk_size
 
-  remote_access {
-    ec2_ssh_key               = var.key_pair               
-    source_security_group_ids = local.app_lt_sg
+  launch_template {
+    id      = aws_launch_template.eks_app_launch_template.id
+    version = "$Latest"
   }
-
 
   scaling_config {
     desired_size = var.node_group_app_desired_size
     max_size     = var.node_group_app_max_size
     min_size     = var.node_group_app_min_size
-   }
+  }
 
   labels = {
-  "Name" = "${local.node_group_name}-app"
-}
+    "Name" = "${local.node_group_name}-app"
+  }
 
   taint {
-    key    =  var.app_taint_key
+    key    = var.app_taint_key
     value  = var.app_taint_value
     effect = var.app_taint_effect
   }
@@ -196,34 +231,72 @@ resource "aws_eks_node_group" "app_node_group" {
 
 ###################### DB Node Group  ####################
 
+resource "aws_launch_template" "eks_db_launch_template" {
+  name_prefix   = local.db_lt_name
+  key_name      = var.key_pair != "" ? var.key_pair : null
+  instance_type = var.db_instance_type
+
+
+  network_interfaces {
+    associate_public_ip_address = var.associate_public_ip_db
+    security_groups             = local.db_lt_sg
+  }
+
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name  = "${local.node_group_name}-db"
+      env   = var.env
+      owner = var.owner
+    }
+  }
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size           = var.ebs_db_volume_size
+      volume_type           = var.ebs_db_volume_type
+      delete_on_termination = var.delete_on_termination_db
+      encrypted             = var.db_encrypted
+    }
+  }
+  tags = {
+    Name  = "${local.node_group_name}-db"
+    env   = var.env
+    owner = var.owner
+  }
+  depends_on = [aws_security_group.sg]
+}
+
+
+
 resource "aws_eks_node_group" "db_node_group" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "${local.node_group_name}-db"
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = var.database_subnet_ids
 
-  ami_type = var.ami_type
+  ami_type      = var.ami_type
   capacity_type = var.db_capacity_type
-  instance_types = var.db_instance_type
-  disk_size      = var.db_disk_size
 
-  remote_access {
-    ec2_ssh_key               = var.key_pair               
-    source_security_group_ids = local.db_lt_sg
+  launch_template {
+    id      = aws_launch_template.eks_db_launch_template.id
+    version = "$Latest"
   }
- 
+
   scaling_config {
     desired_size = var.node_group_db_desired_size
     max_size     = var.node_group_db_max_size
     min_size     = var.node_group_db_min_size
   }
 
-   labels = {
-  "Name" = "${local.node_group_name}-db"
-}
+  labels = {
+    "Name" = "${local.node_group_name}-db"
+  }
 
   taint {
-    key    =  var.db_taint_key
+    key    = var.db_taint_key
     value  = var.db_taint_value
     effect = var.db_taint_effect
   }
@@ -233,5 +306,5 @@ resource "aws_eks_node_group" "db_node_group" {
     env   = var.env
     owner = var.owner
   }
-  depends_on = [ aws_security_group.sg ]
+  depends_on = [aws_security_group.sg]
 }
