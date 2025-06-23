eef02777719dba636e09d45ad73f8ab1ca26420d
Aayush Verma <aayush.verma@mygurukulam.co>
2025-06-23 23:56:41 +0530

    Update variable.tf

--- wrapper-code/eks/variable.tf
+++ wrapper-code/eks/variable.tf
@@ -110,6 +110,10 @@ variable "public_subnet_indexes" {
 
 #################### VPC Peering ########################
 
+variable "use_manage_vpc_data" {
+  type    = bool
+}
+
 variable "peering_connection" {
   type = bool
 }
