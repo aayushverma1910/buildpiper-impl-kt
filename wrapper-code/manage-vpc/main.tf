5c9cb05deb99264a0d45de866bab88c43f14ee5d
Aayush Verma <aayush.verma@mygurukulam.co>
2025-06-23 23:46:23 +0530

    Update main.tf

--- wrapper-code/manage-vpc/main.tf
+++ wrapper-code/manage-vpc/main.tf
@@ -37,7 +37,7 @@ module "networking_module" {
   manage_vpc         = var.manage_vpc
   public_rt_name     = var.public_rt_name
   private_rt_name    = var.private_rt_name
-  peer_region   = var.vpc_peer_region
+  peer_region   = var.peer_region
   peer_owner_id = var.peer_owner_id
 
   #################### Security Groups ########################
