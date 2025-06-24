4300cf586145767b2bc3332f73fb0eb923f61627
Aayush Verma <aayush.verma@mygurukulam.co>
2025-06-24 11:58:10 +0530

    Update main.tf

--- terraform/module/Network-skeleton/main.tf
+++ terraform/module/Network-skeleton/main.tf
@@ -123,8 +123,10 @@ resource "aws_route_table_association" "private_rt_association" {
 
 resource "aws_vpc_peering_connection" "vpc_peering" {
   count       = var.peering_connection ? 1 : 0
+  peer_owner_id = var.peer_owner_id
   peer_vpc_id = aws_vpc.otms_vpc.id
   vpc_id      = var.use_hardcoded_vpc_id ? var.hardcoded_vpc_id : data.aws_vpc.manage_vpc[0].id
+  peer_region   = var.peer_region
   auto_accept = var.vpc_accept
 }
 
